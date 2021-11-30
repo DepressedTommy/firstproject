81d34879e60c20a0f585b38c925491e8997fe56e
mooddie <justincraftpthd2@gmail.com>
2021-11-30 11:57:58 +0000

    Second commit

---
+++ views.py
@@ -0,0 +1,16 @@
+from flask import Blueprint, render_template
+
+views = Blueprint(__name__, "views")
+
+
+@views.route("/")
+def home():
+    return render_template("home.html")
+
+@views.route("/pets")
+def pets():
+    return render_template("pets.html")
+
+@views.route("/shop")
+def shop():
+    return render_template("shop.html")
