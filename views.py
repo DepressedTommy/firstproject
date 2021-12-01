f1575bc2322547e1d3436d9d8b025a2a92ab3343
mooddie <justincraftpthd2@gmail.com>
2021-12-01 00:42:07 +0000

    Sixth commit

--- views.py
+++ views.py
@@ -6,11 +6,3 @@ views = Blueprint(__name__, "views")
 @views.route("/")
 def home():
     return render_template("index.html")
-
-@views.route("/pets")
-def pets():
-    return render_template("pets.html")
-
-@views.route("/shop")
-def shop():
-    return render_template("shop.html")
